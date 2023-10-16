f96551b7febdf43c64879533d6467a4f47659574
Tox2401 <118939727+Tox2401@users.noreply.github.com>
2023-10-16 17:53:04 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+morse_code = {'A': '.-', 'B': '-...', 'C': '-.-.',
+              'D': '-..', 'E': '.', 'F': '..-.',
+              'G': '--.', 'H': '....', 'I': '..',
+              'J': '.---', 'K': '-.-', 'L': '.-..',
+              'M': '--', 'N': '-.', 'O': '---',
+              'P': '.--.', 'Q': '--.-', 'R': '.-.',
+              'S': '...', 'T': '-', 'U': '..-',
+              'V': '...-', 'W': '.--', 'X': '-..-',
+              'Y': '-.--', 'Z': '--..',
+              '1': '.----', '2': '..---', '3': '...--',
+              '4': '....-', '5': '.....', '6': '-....',
+              '7': '--...', '8': '---..', '9': '----.',
+              '0': '-----',
+              }
+
+user_input = input("Which message to you want to convert to morse code?\n")
+morse_message = ""
+for letter in user_input:
+    if letter.upper() in morse_code.keys():
+        morse_message += morse_code[letter.upper()] + " "
+    else:
+        morse_message += "\t"
+
+print(morse_message)
